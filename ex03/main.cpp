ad018a0075bfcdac85744fb8442397649fbd6198
Fernando Manzanares Montes <fmanzana@student.42malaga.com>
2023-07-01 13:17:06 +0200

    make fixed and bsp working perfectly

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,18 +6,17 @@
 /*   By: fmanzana <fmanzana@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 20:40:23 by fmanzana          #+#    #+#             */
-/*   Updated: 2023/06/30 20:53:50 by fmanzana         ###   ########.fr       */
+/*   Updated: 2023/07/01 13:16:43 by fmanzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
 int	main(void) {
-	// Triangle 1:
 	Point v1(0, 0);
-	Point v2(0, 2);
-	Point v3(2, 1);
-	Point p(1, 0.5);
+	Point v2(2, 0);
+	Point v3(1, 2);
+	Point p(1, 1);
 
 	bool res = bsp(v1, v2, v3, p);
 	std::cout << res << "\n";
